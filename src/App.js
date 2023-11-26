eed6d26352890625be12b19c8299211a70443735
akamohara <atsushi.kamohara@hapins.net>
2023-11-26 15:00:31 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,215 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
+
+import "./App.scss";
+import "./index.scss";
+import { playList } from "./songs.js";
+import PauseIcon from "@mui/icons-material/Pause";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+import { PlayArrow } from "@mui/icons-material";
 
 function App() {
+  const [isPlay, setPlayFlag] = useState(false);
+  const [Icon, setIcon] = useState(
+    <PlayArrow className="skipNextButton playerButton" />
+  );
+  const songs = playList();
+  const [index, setIndex] = useState(0);
+  const [duration, setDuration] = useState("");
+  const [active, setActive] = useState(false);
+  const audioRef = useRef(!null);
+  const intervalRef = useRef(null);
+
+  const playListOrder = (props) => {
+    if (props === "next" && index < songs.length - 1) {
+      const newIndex = index + 1;
+      setIndex(newIndex);
+      return newIndex;
+    } else if (props === "previous" && index > 0) {
+      const newIndex = index - 1;
+      setIndex(newIndex);
+      return newIndex;
+    } else {
+      return index;
+    }
+  };
+
+  const showDuration = () => {
+    return (
+      <>
+        <h3 className="duration">
+          {Math.floor(duration / 60)
+            .toString()
+            .padStart(2, "0")}
+          :
+          {Math.floor(duration % 60)
+            .toString()
+            .padStart(2, "0")}
+        </h3>
+      </>
+    );
+  };
+
+  const setPlayIcon = () => {
+    setIcon(<PlayArrow className="playArrowButton playerButton" />);
+  };
+  const setPauseIcon = () => {
+    setIcon(<PauseIcon className="pauseButton playerButton" />);
+  };
+
+  const setSongInfo = () => {
+    return (
+      <>
+        <audio
+          className="audio"
+          ref={audioRef}
+          preload="metadata"
+          onEnded={() => {
+            setPlayIcon();
+            setPlayFlag(false);
+          }}
+        ></audio>
+
+        <div className="songContainer">
+          <img className="coverImg" alt="cover" src={songs[index].cover} />
+          <h2 className="title">{songs[index].title}</h2>
+          <h3 className="artist">{songs[index].artist}</h3>
+          {showDuration()}
+        </div>
+      </>
+    );
+  };
+
+  const skipPreviousButton = () => {
+    return (
+      <SkipPreviousIcon
+        type="button"
+        className="skipPreviousButton playerButton"
+        onClick={() => {
+          audioRef.current.pause();
+          setPlayFlag(false);
+
+          audioRef.current.src = songs[playListOrder("previous")].audio;
+          audioRef.current.play();
+          setPlayFlag(true);
+          setPauseIcon();
+        }}
+      ></SkipPreviousIcon>
+    );
+  };
+
+  const playButton = () => {
+    if (!isPlay) {
+      intervalRef.current = setInterval(() => {
+        setDuration(Math.floor(audioRef.current.currentTime));
+      }, 1000);
+      return (
+        <>
+          <button
+            type="button"
+            onClick={() => {
+              audioRef.current.volume = 0.3;
+              audioRef.current.src = songs[index].audio;
+              audioRef.current.play();
+              setPlayFlag(true);
+              setPauseIcon();
+            }}
+          >
+            {Icon}
+          </button>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <button
+            type="button"
+            onClick={() => {
+              audioRef.current.pause();
+              setPlayFlag(false);
+              setPlayIcon();
+            }}
+          >
+            {Icon}
+          </button>
+        </>
+      );
+    }
+  };
+
+  const skipNextAction = () => {
+    audioRef.current.pause();
+    setPlayFlag(false);
+
+    audioRef.current.src = songs[playListOrder("next")].audio;
+    audioRef.current.play();
+    setPlayFlag(true);
+    setPauseIcon();
+  };
+
+  const skipNextButton = () => {
+    return (
+      <>
+        <SkipNextIcon
+          type="button"
+          className="skipNextButton playerButton"
+          onClick={skipNextAction}
+        ></SkipNextIcon>
+      </>
+    );
+  };
+
+  const setDrawerActive = () => {
+    setActive(!active);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div
+        className={`drawerHandle ${active ? "openDrawer" : ""}`}
+        onClick={() => setDrawerActive()}
+      ></div>
+      <div className={`drawerContainer ${active ? "openDrawer" : ""}`}>
+        <div className="drawer">
+          <p className="playListTitle">PLAY LIST</p>
+          <ul className="playList">
+            {songs.map((info, i) => (
+              <li className="listItem" key={i}>
+                <button
+                  type="button"
+                  className="button"
+                  onClick={() => {
+                    audioRef.current.volume = 0.3;
+                    audioRef.current.src = songs[i].audio;
+                    audioRef.current.play();
+                    setPlayFlag(true);
+                    setIndex(i);
+                    setPauseIcon();
+                  }}
+                >
+                  <img className="cover" src={info.cover} alt="cover"></img>
+                  <div className="infoDiv">
+                    <p className="title">{info.title}</p>
+                    <p className="artist">{info.artist}</p>
+                  </div>
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="mainContainer">
+        {setSongInfo()}
+
+        <div className="playerContainer">
+          {skipPreviousButton()}
+
+          {playButton()}
+
+          {skipNextButton()}
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/songs.js
@@ -0,0 +1,57 @@
+export const playList = () => {
+  return [
+    {
+      title: "2:23 AM",
+      artist: "しゃろう",
+      audio: "./songs/2_23_AM/2_23_AM.mp3",
+      cover: "./songs/2_23_AM/2_23_AM_cover.png",
+      active: true,
+    },
+    {
+      title: "10℃",
+      artist: "しゃろう",
+      audio: "./songs/10℃/10℃.mp3",
+      cover: "./songs/10℃/10℃_cover.png",
+      active: false,
+    },
+    {
+      title: "Cassette Tape Dream",
+      artist: "しゃろう",
+      audio: "./songs/Cassette_Tape_Dream/Cassette_Tape_Dream.mp3",
+      cover: "./songs/Cassette_Tape_Dream/Cassette_Tape_Dream_cover.png",
+      active: false,
+    },
+    {
+      title: "You and Me",
+      artist: "しゃろう",
+      audio: "./songs/You_and_Me/You_and_Me.mp3",
+      cover: "./songs/You_and_Me/You_and_Me_cover.png",
+      active: false,
+    },
+    {
+      title: "サンタは中央線でやってくる",
+      artist: "しゃろう",
+      audio:
+        "./songs/サンタは中央線でやってくる/サンタは中央線でやってくる.mp3",
+      cover:
+        "./songs/サンタは中央線でやってくる/サンタは中央線でやってくる_cover.png",
+      active: false,
+    },
+    {
+      title: "ローファイ少女は今日も寝不足",
+      artist: "しゃろう",
+      audio:
+        "./songs/ローファイ少女は今日も寝不足/ローファイ少女は今日も寝不足.mp3",
+      cover:
+        "./songs/ローファイ少女は今日も寝不足/ローファイ少女は今日も寝不足_cover.png",
+      active: false,
+    },
+    {
+      title: "野良猫は宇宙を目指した",
+      artist: "しゃろう",
+      audio: "./songs/野良猫は宇宙を目指した/野良猫は宇宙を目指した.mp3",
+      cover: "./songs/野良猫は宇宙を目指した/野良猫は宇宙を目指した_cover.png",
+      active: false,
+    },
+  ];
+};
